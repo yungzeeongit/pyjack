13ffafc54d456bc6705a0b8ec25fba417e134f4b
yungzeeongit <greyw4300@gmail.com>
2023-06-21 03:29:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+import random
+cards = []
 suits = ["spades", "clubs", "hearts", "diamonds"]
-suit = suits[2]
-rank = "K"
-value = 10
+ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","k"]
+for suit in suits:
+    for rank in ranks:
+     cards.append([suit, rank])
 
-print(f"Your card is: {rank} of {suit}")
+
+def shuffle_cards():
+   random.shuffle(cards)
+
+def deal_cards(number):
+    cards_dealt = []
+
+    for x in range(number):
+         card = cards.pop()
+         cards_dealt.append(card)
+    return cards_dealt
+
+shuffle_cards()
+
+cards_dealt = deal_cards(2)
+card = cards_dealt[0]
+rank = card[1]
+if rank == "A":
+    value = 11
+elif rank == "J" or rank == "K" or rank == "Q":
+    value = 10
+else:
+    value = rank    
+
+rank_dict = {"rank": rank, "value": value}
+
+print(value, rank)        
